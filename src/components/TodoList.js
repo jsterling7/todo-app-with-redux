fa40b649b2e8523a0c20c839b87eb359f336b0c5
Joshua Sterling <joshua_m_sterling@homedepot.com>
2019-09-25 15:51:29 -0400

    removing log

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,6 @@ import Todo from './Todo';
 
 
 const TodoList = ({ todos, toggleTodo, deleteTodo }) => {
-    console.log(todos);
     const todosList = todos.map(todo => <li key={todo.id} className='list-group-item'><Todo todo={todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/></li>);
     return (
          <div>
