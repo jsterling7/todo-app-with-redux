fd46b2b72eb5fb12219059c8908a43c5d0096471
Joshua Sterling <joshua_m_sterling@homedepot.com>
2019-09-25 10:22:12 -0400

    adding bootstrap

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,9 +3,13 @@ import Todo from './Todo';
 
 
 const TodoList = ({ todos, toggleTodo }) => {
-    const todosList = todos.map(todo => <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo}/>);
+    const todosList = todos.map(todo => <li><Todo key={todo.id} todo={todo} toggleTodo={toggleTodo}/></li>);
     return (
-         <div>{todosList}</div>
+         <div>
+             <ul>
+                {todosList}
+             </ul>
+         </div>
     )
 };
 
